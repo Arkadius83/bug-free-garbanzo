a2f4c205c06adc8a983ff43b25404f555a026451
Arkadius83 <ziele83dj@gmail.com>
2026-08-24 13:21:22 +0200

    Add tasks calendar and safe local AI agents

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
 import "./audio-analysis.css";
+import "./tasks.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
